50734f84c6ac1dcdf64a4d4d97ac507a8ac75596
Rank Preet <preetrank53@gmail.com>
2025-04-02 17:35:37 +0530

    Time: 1 ms (29.82%) | Memory: 44.6 MB (33.47%) - LeetSync

---
+++ 1677-matrix-diagonal-sum/matrix-diagonal-sum.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int diagonalSum(int[][] mat) {
+        int n = mat.length;
+        int sum = 0 ; 
+
+        for(int i = 0 ; i<n ; i++) {
+            for(int j =0 ; j<n ; j++) {
+                int num = mat[i][j];
+
+                if(i==j || i+j==n-1) {
+                    sum=sum+num;
+                }
+            }
+        }
+
+        return sum;
+        
+    }
+}
