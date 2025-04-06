7e582cbed4d9841982cb1e01545d84d0ba9890e7
Rank Preet <preetrank53@gmail.com>
2025-04-06 10:05:36 +0530

    Time: 384 ms (15.16%) | Memory: 40.9 MB (74.95%) - LeetSync

---
+++ 1418-fair-distribution-of-cookies/fair-distribution-of-cookies.java
@@ -0,0 +1,38 @@
+class Solution {
+
+    int minUnfair = Integer.MAX_VALUE;
+    void result(int[] cookies , int[] child , int idx , int k) {
+
+        // base case
+        if(idx==cookies.length) {
+
+            int max = 0;
+            for (int i = 0; i < child.length; i++) {
+                max = Math.max(max, child[i]);
+            }
+
+            minUnfair = Math.min(minUnfair , max);        
+            return;    
+
+        }
+        
+
+        for(int i = 0 ; i<k ; i++) {
+            child[i] = child[i] + cookies[idx];
+
+            // explore
+            result(cookies , child , idx+1 , k);
+
+            //backtrack
+            child[i] = child[i] - cookies[idx];
+        }
+    }
+
+    public int distributeCookies(int[] cookies, int k) {
+        int n = cookies.length;
+        int[] child = new int[k];
+
+        result(cookies , child , 0 , k);
+        return minUnfair;
+    }
+}
