0745e610a6b4a45381460ba9417d19cd53a55897
Rank Preet <preetrank53@gmail.com>
2025-06-24 18:19:37 +0530

    Time: 0 ms (100.00%) | Memory: 56.3 MB (59.66%) - LeetSync

--- 882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.java
+++ 882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.java
@@ -3,19 +3,20 @@ class Solution {
         int n = arr.length;
         int l = 0;
         int r = n-1;
+        int ans = 0;
         
 
         while(l<=r) {
             int mid = l + (r-l)/2;
 
             if(arr[mid] > arr[mid+1]) {
-                
+                ans = mid;
                 r = mid-1;
             }
             else {
                 l = mid + 1;
             }
         }
-        return l;
+        return ans;
     }
 }
