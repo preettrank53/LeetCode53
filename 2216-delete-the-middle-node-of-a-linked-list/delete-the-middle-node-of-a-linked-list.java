3ba8835fc4613754d5363b16d775f6bc8ae7997b
Rank Preet <preetrank53@gmail.com>
2025-06-28 15:26:53 +0530

    Time: 3 ms (99.64%) | Memory: 63.2 MB (76.61%) - LeetSync

--- 2216-delete-the-middle-node-of-a-linked-list/delete-the-middle-node-of-a-linked-list.java
+++ 2216-delete-the-middle-node-of-a-linked-list/delete-the-middle-node-of-a-linked-list.java
@@ -1,47 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
 class Solution {
     public ListNode deleteMiddle(ListNode head) {
-        // Edge case: only one node
-        if (head == null || head.next == null) {
+
+        if(head==null || head.next==null) {
             return null;
         }
 
-        int count = 1;
-        ListNode node = head;
-
-        // Count the number of nodes after head
-        while (node.next != null) {
-            node = node.next;
-            count++;
-        }
-
-        int midIdx;
-        if (count % 2 == 0) {
-            midIdx = count / 2;
-        } else {
-            midIdx = (count - 1) / 2;
-        }
-
-        // Edge case: if midIdx == 0, delete head.next
-        if (midIdx == 0) {
-            head.next = head.next.next;
-            return head;
-        }
-
-        // Reset to head for second traversal
-        count = 0;
-        node = head;
+        ListNode slow = head;
+        ListNode fast = head;
         ListNode prev = null;
 
-        while (node != null) {
-            if (count == midIdx) {
-                prev.next = node.next;
-                break;
-            }
-            prev = node;
-            node = node.next;
-            count++;
+        while(fast != null && fast.next != null) {
+            prev = slow;
+            slow = slow.next;
+            fast = fast.next.next;
         }
-
+        prev.next = slow.next;
         return head;
     }
 }
