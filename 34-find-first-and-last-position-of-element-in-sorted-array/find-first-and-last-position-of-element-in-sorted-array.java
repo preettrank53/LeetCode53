2553c19ee72aad7328b36aa2139fc49b1af9fe3e
Rank Preet <preetrank53@gmail.com>
2025-07-07 18:16:47 +0530

    Time: 0 ms (100.00%) | Memory: 45.9 MB (67.91%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,58 +1,59 @@
 class Solution {
-    // for find right most
-    int findRightMost(int[] nums , int target , int n) {
-        int right_most = -1;
 
-        int l = 0;
-        int r = n-1;
-
-        while(l<=r) {
-            int mid = l + (r-l)/2;
-
-            if(nums[mid]==target) {
-                right_most = mid;
-                l = mid + 1 ;
-            }
-            else if (nums[mid] < target) {
-                l = mid + 1 ;
-            }
-            else {
-                r = mid - 1 ;
-            }
-        }
-        return right_most;
-    }
-
-    // for find left most
-    int findLeftMost(int[] nums , int target , int n) {
-        int left_most = -1;
-
-        int l = 0;
-        int r = n-1;
-
-        while(l<=r) {
-            int mid = l + (r-l)/2;
-
-            if(nums[mid]==target) {
-                left_most = mid;
-                r = mid - 1 ;
-            }
-            else if (nums[mid] < target) {
-                l = mid + 1 ;
-            }
-            else {
-                r = mid - 1 ;
-            }
-        }
-        return left_most;
-    }
-
-    public int[] searchRange(int[] nums, int target) {
+    int startIdx(int[] nums , int target) {
         int n = nums.length;
 
-        int left_most = findLeftMost(nums , target , n) ;
-        int right_most = findRightMost(nums , target , n) ;
+        int l = 0;
+        int r = n-1;
+        int ans = -1;
 
-        return new int[]{left_most , right_most};
+        while(l<=r) {
+            int mid = l + (r-l)/2;
+
+            if(nums[mid]==target) {
+                ans=mid;
+                r=mid-1;
+            }
+            else if(nums[mid] < target) {
+                l = mid+1;
+            }
+            else {
+                r = mid-1;
+            }
+        }
+        return ans;
+    }
+
+
+    int endIdx(int[] nums , int target) {
+        int n = nums.length;
+
+        int l = 0;
+        int r = n-1;
+        int ans = -1;
+
+        while(l<=r) {
+            int mid = l + (r-l)/2;
+
+            if(nums[mid]==target) {
+                ans=mid;
+                l = mid+1;
+            }
+            else if(nums[mid] < target) {
+                l = mid+1;
+            }
+            else {
+                r = mid-1;
+            }
+        }
+        return ans;
+    }
+
+
+    public int[] searchRange(int[] nums, int target) {
+        int leftIdx = startIdx(nums, target);
+        int rightIdx = endIdx(nums, target);
+
+        return new int[]{leftIdx , rightIdx};
     }
 }
