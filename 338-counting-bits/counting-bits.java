830f300ef97ba5ec3ed9d2f90aec92b68772c261
Rank Preet <preetrank53@gmail.com>
2025-07-09 19:04:49 +0530

    Time: 5 ms (24.96%) | Memory: 46.4 MB (93.59%) - LeetSync

---
+++ 338-counting-bits/counting-bits.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int[] countBits(int n) {
+        int[] ans = new int[n+1];
+        ans[0] = 0;
+
+        for(int i = 1 ; i <= n ; i++) {
+            int count = 0 ;
+            int temp = i;
+
+            while( temp != 0 ) {
+                if((temp & 1) == 1 ) {
+                    count++;
+                }
+                temp >>= 1;
+            }
+
+            ans[i] = count;
+
+        }
+        return ans;
+    }
+}
