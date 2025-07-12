993b3e6966203204d591bf0f96e902a6c9195366
Rank Preet <preetrank53@gmail.com>
2025-07-12 18:31:52 +0530

    Time: 0 ms (100.00%) | Memory: 47.7 MB (63.29%) - LeetSync

--- 222-count-complete-tree-nodes/count-complete-tree-nodes.java
+++ 222-count-complete-tree-nodes/count-complete-tree-nodes.java
@@ -1,41 +1,12 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
+
 class Solution {
+
+    int totalNode(TreeNode root) {
+        if (root == null) return 0;
+        return 1 + totalNode(root.left) + totalNode(root.right);
+    }
+
     public int countNodes(TreeNode root) {
-
-        if(root==null) return 0;
-        
-        Queue<TreeNode> queue = new LinkedList<>();
-
-        queue.add(root);
-        int count = 0 ;
-
-        while(!queue.isEmpty()) {
-            int n = queue.size();
-
-            for(int i = 0 ; i<n ; i++) {
-                TreeNode node = queue.poll();
-                count++;
-
-                if(node.left!=null) queue.add(node.left);
-                if(node.right!=null) queue.add(node.right);
-            }
-        }
-
-        return count;
-
+        return totalNode(root);
     }
 }
