b1cd581b2bd15af1e3f8b386b1b3a60de1920aef
Rank Preet <preetrank53@gmail.com>
2025-07-20 11:13:21 +0530

    Time: 3 ms (96.94%) | Memory: 56.7 MB (50.00%) - LeetSync

---
+++ 1299-k-concatenation-maximum-sum/k-concatenation-maximum-sum.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int kConcatenationMaxSum(int[] arr, int k) {
+        int n = arr.length;
+        int MOD = 1_000_000_007;
+
+        long total = 0;
+        for (int num : arr) {
+            total += num;
+        }
+
+        int maxSum = 0;
+        int curr = 0;
+
+        for (int i = 0; i < n * Math.min(k, 2); i++) {
+            int index = i % n;
+            curr = curr + arr[index];
+
+            if (curr > maxSum) {
+                maxSum = curr;
+            }
+
+            if (curr < 0) {
+                curr = 0;
+            }
+        }
+
+        long result = maxSum;
+        if (k > 2 && total > 0) {
+            result = (result + (k - 2) * total) % MOD;
+        }
+
+        return (int)(result % MOD);
+    }
+}
