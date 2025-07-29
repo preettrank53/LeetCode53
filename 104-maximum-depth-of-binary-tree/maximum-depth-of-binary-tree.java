c87aa4a1e749c3899a65fc7e4d73f2c77d4110f8
Rank Preet <preetrank53@gmail.com>
2025-07-29 18:11:09 +0530

    Time: 0 ms (100.00%) | Memory: 42.8 MB (48.76%) - LeetSync

--- 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.java
+++ 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.java
@@ -1,36 +1,13 @@
 class Solution {
-
-    int l = 0;
-    int r = 0;
-
-    void maxLength(TreeNode root, int level) {
-        if (root == null) {
-            return;
-        }
-
-        // Update max depth
-        if (root.left != null) {
-            l = Math.max(l, level + 1);
-        }
-
-        if (root.right != null) {
-            r = Math.max(r, level + 1);
-        }
-
-        maxLength(root.left, level + 1);
-        maxLength(root.right, level + 1);
-    }
-
     public int maxDepth(TreeNode root) {
-        if (root == null) return 0;
-        if (root.left==null && root.right==null) {
-            return 1;
+        if(root == null) {
+            return 0;
         }
 
-        // Start from root at level 1
-        maxLength(root, 1);
+        int leftSum = maxDepth(root.left);
+        int rightSum = maxDepth(root.right);
 
-        // Add +1 to include root level
-        return Math.max(l, r);
+        return Math.max(leftSum , rightSum ) + 1 ;
     }
+
 }
