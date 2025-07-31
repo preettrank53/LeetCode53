d1480b0ee2a0c93a9048fae0c11a4d89e7c32138
Rank Preet <preetrank53@gmail.com>
2025-07-31 22:35:23 +0530

    Time: 0 ms (100.00%) | Memory: 44.5 MB (56.13%) - LeetSync

---
+++ 530-minimum-absolute-difference-in-bst/minimum-absolute-difference-in-bst.java
@@ -0,0 +1,29 @@
+class Solution {
+
+    int minDiff = Integer.MAX_VALUE;
+    TreeNode prev = null;
+
+    void helper(TreeNode node) {
+        if(node == null) {
+            return;
+        }
+
+        if(node.left != null) {
+            helper(node.left);
+        }
+        if(prev != null) {
+            minDiff = Math.min(minDiff , node.val - prev.val);
+        }
+        prev = node;
+
+        if(node.right != null) {
+            helper(node.right);
+        }
+
+    }
+
+    public int getMinimumDifference(TreeNode root) {
+        helper(root);
+        return minDiff;
+    }
+}
