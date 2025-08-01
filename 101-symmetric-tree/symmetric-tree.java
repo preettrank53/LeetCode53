7cf9187436d86d92f04f80438f923c5f5e72b52e
Rank Preet <preetrank53@gmail.com>
2025-08-02 00:29:49 +0530

    Time: 0 ms (100.00%) | Memory: 41.8 MB (52.84%) - LeetSync

--- 101-symmetric-tree/symmetric-tree.java
+++ 101-symmetric-tree/symmetric-tree.java
@@ -1,40 +1,29 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
 
-    boolean helper(TreeNode root1 , TreeNode root2) {
-        if(root1==null && root2==null) {
+    boolean helper(TreeNode node1 , TreeNode node2) {
+        if(node1 == null && node2 == null) {
+            return true;
+        } 
+        if(node1 == null || node2 == null) {
+            return false;
+        }
+
+        if(node1.val != node2.val) {
+            return false;
+        }
+
+
+        boolean x = helper(node1.left , node2.right);
+        boolean y = helper(node1.right , node2.left);
+
+        if(x == true && y == true) {
             return true;
         }
-        if(root1==null || root2==null) {
-            return false;
-        }
 
-        if(root1.val!=root2.val) {
-            return false;
-        }
-
-        boolean x = helper(root1.left , root2.right);
-        boolean y = helper(root1.right , root2.left);
-
-        return x && y;
+        return false;
     }
 
     public boolean isSymmetric(TreeNode root) {
-        
         return helper(root , root);
     }
 }
