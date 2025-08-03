ca03a257107cdd3237b6aeb5e1318005e00e2748
Rank Preet <preetrank53@gmail.com>
2025-08-03 18:37:18 +0530

    Time: 1 ms (100.00%) | Memory: 45.2 MB (35.80%) - LeetSync

--- 654-maximum-binary-tree/maximum-binary-tree.java
+++ 654-maximum-binary-tree/maximum-binary-tree.java
@@ -1,64 +1,28 @@
 class Solution {
 
-    TreeNode helper(List<Integer> leftL, List<Integer> rightL, int rootVal) {
-        TreeNode node = new TreeNode(rootVal);
 
-        if (!leftL.isEmpty()) {
-            int maxL = Integer.MIN_VALUE, idx = -1;
-            for (int i = 0; i < leftL.size(); i++) {
-                if (leftL.get(i) > maxL) {
-                    maxL = leftL.get(i);
-                    idx = i;
-                }
-            }
+    public TreeNode helper(int[] nums, int left, int right) {
+        if (left > right) return null;
 
-            List<Integer> ll = leftL.subList(0, idx);
-            List<Integer> lr = leftL.subList(idx + 1, leftL.size());
-
-            node.left = helper(new ArrayList<>(ll), new ArrayList<>(lr), maxL);
-        }
-
-        if (!rightL.isEmpty()) {
-            int maxR = Integer.MIN_VALUE, idx = -1;
-            for (int i = 0; i < rightL.size(); i++) {
-                if (rightL.get(i) > maxR) {
-                    maxR = rightL.get(i);
-                    idx = i;
-                }
-            }
-
-            List<Integer> rl = rightL.subList(0, idx);
-            List<Integer> rr = rightL.subList(idx + 1, rightL.size());
-
-            node.right = helper(new ArrayList<>(rl), new ArrayList<>(rr), maxR);
-        }
-
-        return node;
-    }
-
-    public TreeNode constructMaximumBinaryTree(int[] nums) {
-        int n = nums.length;
-        int index = -1;
         int maxVal = Integer.MIN_VALUE;
-
-        for (int i = 0; i < n; i++) {
+        int index = -1;
+        for (int i = left; i <= right; i++) {
             if (nums[i] > maxVal) {
                 maxVal = nums[i];
                 index = i;
             }
         }
 
-        List<Integer> list1 = new ArrayList<>();
-        List<Integer> list2 = new ArrayList<>();
-
-        for (int i = 0; i < index; i++) {
-            list1.add(nums[i]);
-        }
-
-        for (int i = index + 1; i < n; i++) {
-            list2.add(nums[i]);
-        }
-
-        return helper(list1, list2, maxVal);
+        TreeNode root = new TreeNode(nums[index]);
+        root.left = helper(nums, left, index - 1);
+        root.right = helper(nums, index + 1, right);
+        
+        return root;
     }
+
+    public TreeNode constructMaximumBinaryTree(int[] nums) {
+        return helper(nums, 0, nums.length - 1);
+    }
+
+    
 }
