12e5c183a23aeb288e156f5e2d83fdd470f4a845
Rank Preet <preetrank53@gmail.com>
2025-08-04 18:14:11 +0530

    Time: 17 ms (76.59%) | Memory: 46 MB (89.04%) - LeetSync

--- 1427-all-elements-in-two-binary-search-trees/all-elements-in-two-binary-search-trees.java
+++ 1427-all-elements-in-two-binary-search-trees/all-elements-in-two-binary-search-trees.java
@@ -19,28 +19,14 @@ class Solution {
     }
 
     public List<Integer> getAllElements(TreeNode root1, TreeNode root2) {
-        List<Integer> list1 = new ArrayList<>();
-        List<Integer> list2 = new ArrayList<>();
-        helper(root1 , list1);
-        helper(root2 , list2);
+        List<Integer> list = new ArrayList<>();
+        helper(root1 , list);
+        helper(root2 , list);
 
-        int n = Math.min(list1.size() , list2.size());
-        List<Integer> ans = new ArrayList<>();
-
-        int i = 0, j = 0;
-        while (i < list1.size() && j < list2.size()) {
-            if (list1.get(i) < list2.get(j)) {
-                ans.add(list1.get(i++));
-            } else {
-                ans.add(list2.get(j++));
-            }
-        }
-
-        while (i < list1.size()) ans.add(list1.get(i++));
-        while (j < list2.size()) ans.add(list2.get(j++));
+        Collections.sort(list);
 
 
-        return ans;
+        return list;
 
 
 
