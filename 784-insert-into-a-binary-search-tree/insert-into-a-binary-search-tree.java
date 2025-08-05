a450e5be0a3b6f28e24269e99d3f09e943e8d662
Rank Preet <preetrank53@gmail.com>
2025-08-05 07:34:05 +0530

    Time: 0 ms (100.00%) | Memory: 45.1 MB (82.96%) - LeetSync

--- 784-insert-into-a-binary-search-tree/insert-into-a-binary-search-tree.java
+++ 784-insert-into-a-binary-search-tree/insert-into-a-binary-search-tree.java
@@ -1,32 +1,21 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
+
+    TreeNode helper(TreeNode node , int target) {
+        if(node == null) {
+            return new TreeNode(target);
+        }
+
+        if(node.val > target) {
+            node.left = helper(node.left , target);
+        }
+        if(node.val < target) {
+            node.right = helper(node.right , target);
+        }
+
+        return node;
+    }
+
     public TreeNode insertIntoBST(TreeNode root, int val) {
-        if(root==null) {
-            return new TreeNode(val);
-        }
-
-        if(val < root.val) {
-            root.left = insertIntoBST(root.left , val);
-        }
-
-        else {
-            root.right = insertIntoBST(root.right , val);
-        }
-
-        return root;
+        return helper(root , val);
     }
 }
