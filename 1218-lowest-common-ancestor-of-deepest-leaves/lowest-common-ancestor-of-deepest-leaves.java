81624a359a1a727463a8e289738d96f11cb1f471
Rank Preet <preetrank53@gmail.com>
2025-08-06 08:09:45 +0530

    Time: 0 ms (100.00%) | Memory: 44.3 MB (71.53%) - LeetSync

---
+++ 1218-lowest-common-ancestor-of-deepest-leaves/lowest-common-ancestor-of-deepest-leaves.java
@@ -0,0 +1,39 @@
+class Solution {
+
+    int maxLen = -1;
+
+    TreeNode helper(TreeNode node) {
+        if(node == null) {
+            return null;
+        }
+
+        int left = height(node.left);
+        int right = height(node.right);
+
+
+        if(left > right ) {
+            return helper(node.left);
+        }
+        else if (right > left) {
+            return helper(node.right);
+        }
+
+        return node;
+
+    }
+
+    int height(TreeNode node) {
+        if(node == null) {
+            return 0;
+        }
+
+        int left = height(node.left);
+        int right = height(node.right);
+
+        return Math.max(left , right) + 1;
+    }
+
+    public TreeNode lcaDeepestLeaves(TreeNode root) {
+        return helper(root);
+    }
+}
