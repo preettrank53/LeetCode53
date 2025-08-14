51f30c049bab9145b9c8dff2de20a232ddc679f7
Rank Preet <preetrank53@gmail.com>
2025-08-14 17:53:35 +0530

    Time: 0 ms (100.00%) | Memory: 41.2 MB (46.04%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -1,32 +1,22 @@
 class Solution {
     public int rob(int[] nums) {
         int n = nums.length;
+
         if(n==1) {
             return nums[0];
         }
-
         if(n==2) {
             return Math.max(nums[0] , nums[1]);
         }
 
-        int[] m = new int[n];
-        m[0] = nums[0];
-        m[1] = Math.max(nums[1] , m[0] );
-
-        int[] t = new int[n];
-        t[0] = m[0];
-        t[1] = m[1];
+        int[] dp = new int[n];
+        dp[0] = nums[0]; 
+        dp[1] = Math.max(nums[1] , nums[0]); 
 
         for(int i = 2 ; i<n ; i++) {
-            m[i] = Math.max(m[i-1] , nums[i] + m[i-2]);
+            dp[i] = Math.max(dp[i-1], nums[i] + dp[i-2]);
         }
 
-        // for(int i = 2 ; i<n ; i++) {
-        //     t[i] = nums[i] + m[i-2];
-        //     m[i] = Math.max(t[i] , m[i]);
-        // }
-
-        return m[n-1];
+        return Math.max(dp[n-1] , dp[n-2]);
     }
 }
-
