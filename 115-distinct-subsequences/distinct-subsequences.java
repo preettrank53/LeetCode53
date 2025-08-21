c1a7a887eff96972c21c85670d7482153e5c822d
Rank Preet <preetrank53@gmail.com>
2025-08-21 18:20:34 +0530

    Time: 7 ms (97.96%) | Memory: 51.5 MB (30.55%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.java
+++ 115-distinct-subsequences/distinct-subsequences.java
@@ -1,36 +1,30 @@
 class Solution {
-
-    int[][] memo;
-    
-    int helper(String s , String t , int i , int j) {
-        if(j < 0) {
-            return 1;
-        }
-        if(i < 0) {
-            return 0;
-        }
-
-        if(memo[i][j] != -1) {
-            return memo[i][j];
-        }
-
-        if(s.charAt(i) == t.charAt(j)) {
-            return memo[i][j] = helper(s , t , i-1 , j-1) + helper(s ,t , i-1 , j);
-        }
-        return memo[i][j] = helper(s , t , i-1 , j);
-    }
-
     public int numDistinct(String s, String t) {
-        int n = s.length();
-        int m = t.length();
-        memo = new int[n][m];
+        char[] ch1 = s.toCharArray();
+        char[] ch2 = t.toCharArray();
 
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                memo[i][j] = -1;
+        int n = ch1.length;
+        int m = ch2.length;
+
+        int[][] dp = new int[n+1][m+1];
+        // you already know what is this if you follow striver playlist
+        for (int i = 0; i <n+1; i++) {
+            dp[i][0] = 1;
+        }
+        for (int j = 1; j <m+1; j++) {
+            dp[0][j] = 0;
+        }
+
+        for(int i = 1 ; i<n+1 ; i++) {
+            for(int j = 1 ; j<m+1 ; j++) {
+                if(ch1[i-1] == ch2[j-1]) {
+                    dp[i][j] = dp[i-1][j-1] + dp[i-1][j];
+                }
+                else {
+                    dp[i][j] = dp[i-1][j];
+                }
             }
         }
-                
-        return helper(s , t , n-1 , m-1);
+        return dp[n][m];
     }
 }
