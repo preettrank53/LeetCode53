cdcd34eb168cf394b4f04bd2b8697d189bc69aa9
Rank Preet <preetrank53@gmail.com>
2025-08-27 22:04:47 +0530

    Time: 16 ms (58.76%) | Memory: 55 MB (81.04%) - LeetSync

--- 714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.java
+++ 714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.java
@@ -1,35 +1,24 @@
 class Solution {
-    int ans = 0;
-    int[][] memo;
-    int helper(int[] price , int i , int buy , int fee) {
-
-        if(i==price.length) {
-            return 0;
-        }
-        
-        if(memo[i][buy] != -1) {
-            return memo[i][buy];
-        }
-
-        if(buy == 1) {
-            ans = Math.max(-price[i] + helper(price , i+1 , 0 , fee) , helper(price , i+1 , 1 , fee));
-        }
-        else {
-            ans = Math.max(price[i] + helper(price , i+1 , 1 , fee) - fee , helper(price , i+1 , 0 , fee));
-        }
-
-        return memo[i][buy] = ans;
-    }
-
     public int maxProfit(int[] prices , int fee) {
         int n = prices.length;
-        memo = new int[n][2];
 
-        for(int i = 0 ; i<n ; i++) {
+        int[][] dp = new int[n+1][2];
+        
+        for(int j = 0 ; j<2 ; j++) {
+            dp[n-1][j] = 0; 
+        }
+
+        for(int i = n-1 ; i>=0 ; i--) {
             for(int j = 0 ; j<2 ; j++) {
-                memo[i][j] = -1;
+                if(j == 1) {
+                    dp[i][j] = Math.max(-prices[i] + dp[i+1][0] , dp[i+1][1]);
+                }
+                else {
+                    dp[i][j] = Math.max(prices[i] + dp[i+1][1] - fee , dp[i+1][0]);
+                }
             }
         }
-        return helper(prices , 0 , 1 , fee);
+
+        return dp[0][1];
     }
 }
