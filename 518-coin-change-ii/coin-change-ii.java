81681b015a3b08db575c96a506bd5ee1c127dadc
Rank Preet <preetrank53@gmail.com>
2025-08-27 10:29:08 +0530

    Time: 9 ms (62.59%) | Memory: 49.3 MB (37.63%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.java
+++ 518-coin-change-ii/coin-change-ii.java
@@ -1,35 +1,24 @@
 class Solution {
-
-    int[][] memo;
-    int helper(int[] coins , int amount , int i) {
-        if(i == 0) {
-            if (amount % coins[0] == 0) return 1;
-            return 0;
-        }
-
-        if(memo[i][amount] != -1) {
-            return memo[i][amount];
-        }
-
-        int notTake = helper(coins , amount , i-1);
-        int take = 0;
-        if(coins[i] <= amount) {
-            take = helper(coins , amount - coins[i] , i);
-        }
-
-        return memo[i][amount] = notTake + take;
-    }
-
     public int change(int amount, int[] coins) {
         int n = coins.length;
-        memo = new int[n][amount+1];
+        int[][] dp = new int[n][amount + 1];
 
-        for(int i = 0 ; i<n ; i++) {
-            for(int j = 0 ; j<amount+1 ; j++) {
-                memo[i][j] = -1;
+        for (int j = 0; j <= amount; j++) {
+            if (j % coins[0] == 0) dp[0][j] = 1;
+            else dp[0][j] = 0;
+        }
+
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < amount+1 ; j++) {
+                int notTake = dp[i - 1][j];
+                int take = 0;
+                if (coins[i] <= j) {
+                    take = dp[i][j - coins[i]]; 
+                }
+                dp[i][j] = notTake + take;
             }
         }
 
-        return helper(coins , amount , n-1 );
+        return dp[n - 1][amount];
     }
 }
