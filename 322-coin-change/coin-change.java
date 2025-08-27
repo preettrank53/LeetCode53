e08c74b82f4a3c6c1b503f1e0cf2f8cd5ef9995d
Rank Preet <preetrank53@gmail.com>
2025-08-27 09:17:22 +0530

    Time: 15 ms (77.26%) | Memory: 45.2 MB (30.37%) - LeetSync

--- 322-coin-change/coin-change.java
+++ 322-coin-change/coin-change.java
@@ -1,38 +1,36 @@
 class Solution {
-    int[][] memo;
-    int helper(int[] coins , int amount , int i) {
-        if (amount == 0) return 0;
-        if(i == 0) {
-            if(amount % coins[0] == 0) {
-                return amount/coins[0];
-            }
-            return (int)1e9;
-        }
-
-        if(memo[i][amount] != -1) {
-            return memo[i][amount];
-        }
-
-        int notTake = helper(coins , amount , i-1);
-        int take = (int)1e9;
-        if(coins[i] <= amount) {
-            take = 1 + helper(coins , amount - coins[i] , i);
-        }
-
-        return memo[i][amount] = Math.min(notTake , take);
-    }
-
     public int coinChange(int[] coins, int amount) {
         int n = coins.length;
-        memo = new int[n][amount+1];
+
+        int[][] dp = new int[n][amount+1];
 
         for(int i = 0 ; i<n ; i++) {
-            for(int j = 0 ; j<amount+1 ; j++) {
-                memo[i][j] = -1;
+            dp[i][0] = 0;
+        }
+
+        for(int j = 0 ; j<amount+1 ; j++) {
+            if(j % coins[0] == 0) {
+                dp[0][j] = j / coins[0];
+            }
+            else {
+                dp[0][j] = (int)1e9;
+            }
+            
+        }
+
+        for(int i = 1 ; i<n ; i++) {
+            for(int j = 1 ; j<amount+1 ; j++) {
+                int notTake = dp[i-1][j];
+                int take = (int)1e9;
+                if(coins[i] <= j) {
+                    take = 1 + dp[i][j - coins[i]];
+                }
+
+                dp[i][j] = Math.min(notTake , take);
             }
         }
 
-        int ans =  helper(coins , amount , n-1 );
-        return (ans >= (int)1e9) ? -1 : ans;
+        int ans = dp[n-1][amount];
+        return (ans >= 1e9) ? -1 : ans;
     }
 }
