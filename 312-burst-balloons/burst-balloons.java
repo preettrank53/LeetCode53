1ed089624f3988391cec63df0dca1f76a54e9b4a
Rank Preet <preetrank53@gmail.com>
2025-08-28 22:48:19 +0530

    Time: 33 ms (87.56%) | Memory: 43 MB (30.19%) - LeetSync

--- 312-burst-balloons/burst-balloons.java
+++ 312-burst-balloons/burst-balloons.java
@@ -1,44 +1,41 @@
 class Solution {
-    int[][] memo;
-    int helper(int[] arr , int i , int j) {
-
-        if (i > j) return 0;
-
-        if(memo[i][j] != -1) {
-            return memo[i][j];
-        }
-
-        int ans = 0;
-        int max = Integer.MIN_VALUE;
-        for(int k = i ; k<j+1 ; k++) {
-            ans = arr[i-1] * arr[k] * arr[j+1] + helper(arr , i , k-1) + helper(arr , k+1 , j);
-            if(ans > max) {
-                max = ans ;
-            }
-        }
-
-        return memo[i][j] = max;
-
-
-    }
-
     public int maxCoins(int[] nums) {
         int n = nums.length;
-        int[] arr = new int[n+2];
 
+        int[] arr = new int[n+2];
         arr[0] = 1;
-        arr[n + 1] = 1;
+        arr[n+1] = 1;
+
         for (int i = 0; i < n; i++) {
             arr[i + 1] = nums[i];
         }
 
-        memo = new int[n+2][n+2];
+        int[][] dp = new int[n+2][n+2];
+
         for(int i = 0 ; i<n+2 ; i++) {
             for(int j = 0 ; j<n+2 ; j++) {
-                memo[i][j] = -1;
+                if(i > j) {
+                    dp[i][j] = 0;
+                } 
             }
         }
 
-        return helper(arr , 1 , n);
+        int m = arr.length;
+
+        for(int i = m-2 ; i>=1 ; i--) {
+            for(int j = i ; j<=n ; j++) {
+                int max = Integer.MIN_VALUE;
+                for(int k = i ; k<j+1 ; k++) {
+                    int ans = arr[i-1] * arr[k] * arr[j+1] + dp[i][k-1] + dp[k+1][j];
+                    if(ans > max) {
+                        max = ans;
+                    }
+                }
+
+                dp[i][j] = max;
+            }
+        }
+
+        return dp[1][m-2];
     }
 }
