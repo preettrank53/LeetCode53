af3454d013368ec07f6fe6d6b357f9b639903795
Rank Preet <preetrank53@gmail.com>
2025-08-28 22:56:43 +0530

    Time: 12 ms (79.01%) | Memory: 42.2 MB (93.50%) - LeetSync

--- 1669-minimum-cost-to-cut-a-stick/minimum-cost-to-cut-a-stick.java
+++ 1669-minimum-cost-to-cut-a-stick/minimum-cost-to-cut-a-stick.java
@@ -1,50 +1,35 @@
 class Solution {
-    int[][] memo;
-    int helper(int[] cuts, int i, int j) {
-
-        if (i > j) {
-            return 0;
-        }
-
-        if(memo[i][j] != -1) {
-            return memo[i][j];
-        }
-
-        int ans;
-        int min = Integer.MAX_VALUE;
-
-        for (int x = i; x <= j; x++) {
-            ans = (cuts[j + 1] - cuts[i - 1])   
-                + helper(cuts, i, x - 1)     
-                + helper(cuts, x + 1, j);    
-
-            min = Math.min(min, ans);
-        }
-
-        return memo[i][j] = min;
-    }
-
-    public int minCost(int m, int[] cuts) {
+    public int minCost(int z, int[] cuts) {
         int n = cuts.length;
+        Arrays.sort(cuts);
 
-        memo = new int[n+2][n+2];
-        
-        for(int i = 0 ; i<n+2 ; i++) {
-            for(int j = 0 ; j<n+2 ; j++) {
-                memo[i][j] = -1;
-            }
-        }
+        int[] arr = new int[n+2];
+        arr[0] = 0;
+        arr[n+1] =  z;
 
-        int[] newCuts = new int[n + 2];
-
-        newCuts[0] = 0;
-        newCuts[n + 1] = m;
         for (int i = 0; i < n; i++) {
-            newCuts[i + 1] = cuts[i];
+            arr[i + 1] = cuts[i];
         }
 
-        Arrays.sort(newCuts);
 
-        return helper(newCuts, 1, n); 
+        int[][] dp = new int[n+2][n+2];     
+
+        int m = arr.length;
+
+        for(int i = m-2 ; i>=1 ; i--) {
+            for(int j = i ; j<=n ; j++) {
+                int min = Integer.MAX_VALUE;
+                for(int k = i ; k<j+1 ; k++) {
+                    int ans = ( arr[j+1] - arr[i-1] ) + dp[i][k-1] + dp[k+1][j];
+                    if(ans < min) {
+                        min = ans;
+                    }
+                }
+
+                dp[i][j] = min;
+            }
+        } 
+        
+        return dp[1][m-2];
     }
 }
