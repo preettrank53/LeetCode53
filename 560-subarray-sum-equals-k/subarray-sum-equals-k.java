55a288e3a3eefcf04e93dd0e89815b988dd9d120
Rank Preet <preetrank53@gmail.com>
2025-08-30 21:50:08 +0530

    Time: 1591 ms (5.01%) | Memory: 46.3 MB (76.31%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.java
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.java
@@ -1,23 +1,26 @@
 class Solution {
     public int subarraySum(int[] nums, int k) {
         int n = nums.length;
-        int[] arr = new int[n + 1]; 
+        int[] arr = new int[n+1];  // prefix sum
         arr[0] = 0;
 
         for (int i = 1; i <= n; i++) {
-            arr[i] = arr[i - 1] + nums[i - 1];
+            arr[i] = arr[i-1] + nums[i-1];
         }
 
-        int count = 0;
+        int[] ans = new int[n+1];
+        ans[0] = 0;
 
         for (int i = 1; i <= n; i++) {
+            ans[i] = ans[i-1];
+
             for (int j = 0; j < i; j++) {
                 if (arr[i] - arr[j] == k) {
-                    count++;
+                    ans[i]++;  
                 }
             }
         }
 
-        return count;
+        return ans[n];
     }
 }
