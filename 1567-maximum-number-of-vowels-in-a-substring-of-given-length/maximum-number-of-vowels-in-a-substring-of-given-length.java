de45fa82e4c9638173c06eb061f96afc203375c9
Rank Preet <preetrank53@gmail.com>
2025-09-03 18:01:02 +0530

    Time: 17 ms (56.60%) | Memory: 44.9 MB (42.43%) - LeetSync

--- 1567-maximum-number-of-vowels-in-a-substring-of-given-length/maximum-number-of-vowels-in-a-substring-of-given-length.java
+++ 1567-maximum-number-of-vowels-in-a-substring-of-given-length/maximum-number-of-vowels-in-a-substring-of-given-length.java
@@ -1,35 +1,28 @@
 class Solution {
-
-    boolean isVowel(char ch) {
-        if(ch=='a' || ch=='e' || ch=='i'|| ch=='o' || ch=='u') {
-            return true;
-        }
-        return false;
-    }
-
     public int maxVowels(String s, int k) {
         int n = s.length();
 
         int i = 0 ;
         int j = 0 ;
         int count = 0 ;
-        int maxV = 0;
+        int maxCount = 0 ;
 
-        while(j<n) {
-            if(isVowel(s.charAt(j))) {
+        while(j < n) {
+            if(s.charAt(j) == 'a' || s.charAt(j) == 'e' || s.charAt(j) == 'i' || s.charAt(j) == 'o' || s.charAt(j) == 'u') {
                 count++;
             }
 
-            if(j-i+1==k) {
-                maxV = Math.max(maxV , count);
-                if(isVowel(s.charAt(i))) {
+            while(j - i + 1 == k) {
+                maxCount = Math.max(maxCount , count);
+                if(s.charAt(i) == 'a' || s.charAt(i) == 'e' || s.charAt(i) == 'i' || s.charAt(i) == 'o' || s.charAt(i) == 'u') {
                     count--;
                 }
                 i++;
             }
+
             j++;
         }
 
-        return maxV;
+        return maxCount;
     }
 }
