2445002b964c7e78541aca1183e50414cf2ceb95
Rank Preet <preetrank53@gmail.com>
2025-09-10 18:02:23 +0530

    Time: 21 ms (38.82%) | Memory: 42.7 MB (68.87%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.java
+++ 567-permutation-in-string/permutation-in-string.java
@@ -1,48 +1,49 @@
-import java.util.*;
-
 class Solution {
-
-    boolean allZero(Map<Character, Integer> map) {
-        for (int val : map.values()) {
-            if (val != 0) return false;
-        }
-        return true;
-    }
-
     public boolean checkInclusion(String s1, String s2) {
         int n = s2.length();
         int m = s1.length();
 
-        if (m > n) return false;
+        Map<Character , Integer> map = new HashMap<>();
 
-        char[] arr = s2.toCharArray();
-        Map<Character, Integer> map = new HashMap<>();
-
-        for (int i = 0; i < m; i++) {
+        for(int i = 0 ; i < m ; i++ ) {
             char ch = s1.charAt(i);
-            map.put(ch, map.getOrDefault(ch, 0) + 1);
+            if(map.containsKey(ch)) {
+                map.put(ch , map.get(ch) + 1);
+            }
+            else {
+                map.put(ch , 1);
+            }
         }
 
-        int i = 0, j = 0;
-
-        while (j < n) {
-            if (map.containsKey(arr[j])) {
-                map.put(arr[j], map.get(arr[j]) - 1);
+        int i = 0 ;
+        int j = 0 ;
+        int count = map.size();
+        
+        while(j < n) {
+            char ch = s2.charAt(j);
+            if(map.containsKey(ch)) {
+                map.put(ch , map.get(ch) - 1);
+                if(map.get(ch) == 0) {
+                    count--;
+                }
             }
 
-            if (j - i + 1 == m) {
-                if (allZero(map)) {
-                    return true; 
-                }
-                
-                if (map.containsKey(arr[i])) {
-                    map.put(arr[i], map.get(arr[i]) + 1);
+            while(j-i+1 > m) {
+                char ch1 = s2.charAt(i);
+                if(map.containsKey(ch1)) {
+                    if(map.get(ch1) == 0 ) {
+                        count++;
+                    }
+                    map.put(ch1 , map.get(ch1) + 1);
                 }
                 i++;
             }
+
+            if(count == 0 && (j-i+1) == m) {
+                return true;
+            }
             j++;
         }
-
         return false;
     }
 }
