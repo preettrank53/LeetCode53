95d5ec1b3c64c616d89f2f3af3bdae2dfd28bb74
Rank Preet <preetrank53@gmail.com>
2025-12-29 09:51:55 +0530

    Time: 2 ms (68.39%) | Memory: 44.5 MB (68.36%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.java
+++ 22-generate-parentheses/generate-parentheses.java
@@ -1,43 +1,23 @@
 class Solution {
 
-    boolean isValid(String str) {
-    int balance = 0;
-
-    for (int i = 0; i < str.length(); i++) {
-        char ch = str.charAt(i);
-
-        if (ch == '(') {
-            balance++;
-        } else {
-            balance--;
-        }
-
-        if (balance < 0) {
-            return false; 
-        }
-    }
-
-        return balance == 0; 
-    }
-
-    void helper(int n , String str , List<String> list) {
-        if(str.length() == n) {
-            if(isValid(str)) {
-                list.add(str);
-            }
+    void helper(int open, int close, int n, String str, List<String> list) {
+        if (str.length() == 2 * n) {
+            list.add(str);
             return;
         }
 
-        helper(n , str + '(' , list);
-        helper(n , str + ')' , list);
+        if (open < n) {
+            helper(open + 1, close, n, str + "(", list);
+        }
+
+        if (close < open) {
+            helper(open, close + 1, n, str + ")", list);
+        }
     }
 
     public List<String> generateParenthesis(int n) {
         List<String> list = new ArrayList<>();
-
-        String str = "";
-        helper(2*n , str , list);
-
+        helper(0, 0, n, "", list);
         return list;
     }
 }
