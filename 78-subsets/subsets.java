3f91429a19ab87ae410901079500357be3e518a7
Rank Preet <preetrank53@gmail.com>
2025-12-29 10:16:36 +0530

    Time: 1 ms (86.26%) | Memory: 44 MB (83.66%) - LeetSync

--- 78-subsets/subsets.java
+++ 78-subsets/subsets.java
@@ -1,38 +1,23 @@
 class Solution {
 
-    void printSubSet(List<List<Integer>> list , List<Integer> arr , List<Integer> ans , int i ) {
-        if(i==arr.size()) {
-            list.add(new ArrayList<>(ans));
+    void helper(int[] nums, List<List<Integer>> list, List<Integer> arr, int n) {
+        if (n == nums.length) {
+            list.add(new ArrayList<>(arr));
             return;
         }
 
-        //include
+        arr.add(nums[n]);
+        helper(nums, list, arr, n + 1);
 
-        ans.add(arr.get(i));
-        printSubSet(list,arr,ans,i+1);
-
-        // Backtrack (remove last element)
-        ans.remove(ans.size()-1);
-
-        //exclude
-        printSubSet(list,arr,ans,i+1);
-
+        arr.remove(arr.size() - 1);
+        helper(nums, list, arr, n + 1);
     }
 
     public List<List<Integer>> subsets(int[] nums) {
-
         List<List<Integer>> list = new ArrayList<>();
         List<Integer> arr = new ArrayList<>();
-        
-        // Convert int[] to List<Integer>
-        for(int i = 0 ; i <nums.length ; i++) {
-            arr.add(nums[i]);
-        } 
 
-        List<Integer> ans = new ArrayList<>();
-
-        printSubSet(list , arr , ans , 0);
+        helper(nums, list, arr, 0); 
         return list;
-
     }
 }
