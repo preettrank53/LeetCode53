b3f93d7eef340747a5bfec2ac14c1886e747e0fa
Rank Preet <preetrank53@gmail.com>
2025-12-30 09:37:41 +0530

    Time: 2 ms (95.28%) | Memory: 45.8 MB (48.70%) - LeetSync

--- 39-combination-sum/combination-sum.java
+++ 39-combination-sum/combination-sum.java
@@ -1,38 +1,27 @@
 class Solution {
 
-    void combSum(int[] arr ,int i , int target , Set<List<Integer>> ans , List<Integer> combin ) {
-
-        // Base case
-
-        if(target==0) {
-            ans.add(new ArrayList<>(combin));
+    int sum = 0 ;
+    void helper(int[] candidates , int target , List<List<Integer>> list , List<Integer> ans , int sum , int n) {
+ 
+        if (sum == target) {
+            list.add(new ArrayList<>(ans));
             return;
         }
 
-        if(i==arr.length || target<0) {
+        if (sum > target || n >= candidates.length) {
             return;
         }
 
+        ans.add(candidates[n]);
+        helper(candidates, target, list, ans, sum + candidates[n], n);
+        ans.remove(ans.size() - 1); 
 
-        combin.add(arr[i]);
-        // call for single choise
-        combSum(arr , i+1 , target-arr[i] , ans , combin);
-
-        // call for multiple choise
-        combSum( arr , i , target-arr[i] , ans , combin);
-
-        // Before BackTracking remove last element from combination
-        combin.remove(combin.size()-1);
-        
-        //call for exclude choise
-        combSum( arr , i+1 , target , ans , combin );
+        helper(candidates, target, list, ans, sum, n + 1);
     }
 
-    public List<List<Integer>> combinationSum(int[] arr, int target) {
-        Set<List<Integer>> ans = new HashSet<>();
-        List<Integer> combin = new ArrayList<>();       //combin==combination
-
-        combSum(arr, 0 , target , ans , combin);
-        return new ArrayList<>(ans);
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> list = new ArrayList<>();
+        helper(candidates, target, list, new ArrayList<>(), 0, 0);
+        return list;
     }
 }
