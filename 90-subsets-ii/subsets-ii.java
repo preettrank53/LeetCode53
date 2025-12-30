cc7a2aa7a80db76f01fbc36b77f43d343c7a9274
Rank Preet <preetrank53@gmail.com>
2025-12-30 10:35:51 +0530

    Time: 2 ms (97.44%) | Memory: 45.1 MB (57.35%) - LeetSync

--- 90-subsets-ii/subsets-ii.java
+++ 90-subsets-ii/subsets-ii.java
@@ -1,42 +1,28 @@
 class Solution {
 
-    void printSubSet(Set<List<Integer>> set , List<Integer> arr , List<Integer> ans , int i ) {
-        if(i==arr.size()) {
-
-            set.add(new ArrayList<>(ans));
-            
+    void helper(int[] nums , List<List<Integer>> list  , List<Integer> ans, int n) {
+        if(n == nums.length) {
+            list.add(new ArrayList<>(ans));
             return;
         }
 
-        //include
+        ans.add(nums[n]);
+        helper(nums , list , ans , n+1);
+        ans.remove(ans.size() - 1);
 
-        ans.add(arr.get(i));
-        printSubSet(set,arr,ans,i+1);
-
-        // Backtrack (remove last element)
-        ans.remove(ans.size()-1);
-
-        //exclude
-        printSubSet(set,arr,ans,i+1);
-
+        while(n + 1 < nums.length && nums[n] == nums[n+1]) {
+            n++;
+        }
+        helper(nums , list , ans , n+1);
     }
 
     public List<List<Integer>> subsetsWithDup(int[] nums) {
+        Arrays.sort(nums);
+        int n = nums.length;
+        List<List<Integer>> list = new ArrayList<>();
 
-        Set<List<Integer>> set = new HashSet<>();
-        List<Integer> arr = new ArrayList<>();
+        helper(nums , list , new ArrayList<>() , 0);
 
-        Arrays.sort(nums);  // draw recursion tree for [1,2,2] & [2,1,1] , than see the difference
-        
-        // Convert int[] to List<Integer>
-        for(int i = 0 ; i <nums.length ; i++) {
-            arr.add(nums[i]);
-        } 
-
-        List<Integer> ans = new ArrayList<>();
-
-        printSubSet(set , arr , ans , 0);
-        return new ArrayList<>(set);
-
+        return list;
     }
 }
